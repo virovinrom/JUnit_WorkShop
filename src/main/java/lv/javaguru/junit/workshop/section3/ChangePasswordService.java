414664a6640f6d8adf93866b00a64c128650d798
virovinrom <32594415+virovinrom@users.noreply.github.com>
2018-01-18 10:02:54 +0200

    Delete ChangePasswordService.java

--- src/main/java/lv/javaguru/junit/workshop/section3/ChangePasswordService.java
+++
@@ -1,9 +0,0 @@
-package lv.javaguru.junit.workshop.section3;
-
-import lv.javaguru.junit.workshop.section3.domain.User;
-
-public interface ChangePasswordService {
-
-    void changePassword(User user, String newPassword);
-
-}

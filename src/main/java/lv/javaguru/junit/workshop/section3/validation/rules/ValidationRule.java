49828f56b65a1ee8eeb20cc742c8e27b8e84dfe1
virovinrom <32594415+virovinrom@users.noreply.github.com>
2018-01-18 10:03:19 +0200

    Delete ValidationRule.java

--- src/main/java/lv/javaguru/junit/workshop/section3/validation/rules/ValidationRule.java
+++
@@ -1,9 +0,0 @@
-package lv.javaguru.junit.workshop.section3.validation.rules;
-
-import lv.javaguru.junit.workshop.section3.domain.User;
-
-public interface ValidationRule {
-
-    void validate(User user, String newPassword);
-
-}

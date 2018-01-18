29fcd86d487e957333dec4defcb77385a792c6f7
virovinrom <32594415+virovinrom@users.noreply.github.com>
2018-01-18 10:03:06 +0200

    Delete PasswordValidator.java

--- src/main/java/lv/javaguru/junit/workshop/section3/validation/PasswordValidator.java
+++
@@ -1,9 +0,0 @@
-package lv.javaguru.junit.workshop.section3.validation;
-
-import lv.javaguru.junit.workshop.section3.domain.User;
-
-public interface PasswordValidator {
-
-    void validate(User user, String newPassword);
-
-}
